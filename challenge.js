d5e19b90a228d6c3b2ab74761d68fa46f6526331
Billan Abdi <billanabdi04@gmail.com>
2023-11-09 02:10:25 +0400

    sln

--- challenge.js
+++ challenge.js
@@ -1,2 +1,12 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+function bmi(weight, height) {
+  return weight * height;
+}
+function people(name) {
+  return name;
+}
+
+console.log(people("Moe"));
+console.log(bmi(9, 50));
