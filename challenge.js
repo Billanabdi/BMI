7d350a3fa949160ca47bd6913283adc653115aa6
Billan Abdi <billanabdi04@gmail.com>
2023-11-09 02:19:42 +0400

    Solution

--- challenge.js
+++ challenge.js
@@ -2,11 +2,21 @@
 // Write your code here.
 
 function bmi(weight, height) {
-  return weight * height;
+  const bmi = weight / (height * height);
+  return bmi;
 }
-function people(name) {
-  return name;
-}
+let moeWeight = 90;
+let moeHeight = 2.05;
+const moeBMI = bmi(moeWeight, moeHeight);
 
-console.log(people("Moe"));
-console.log(bmi(9, 50));
+let aliWeight = 60;
+let aliHeight = 1.8;
+const aliBMI = bmi(aliHeight, aliWeight);
+
+if (moeBMI > aliBMI) {
+  console.log("Moe has a bigger BMI");
+} else if (aliBMI > moeBMI) {
+  console.log("Ali has a bigger BMI");
+} else {
+  console.log("They have the same BMI");
+}
